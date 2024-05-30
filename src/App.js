e2aeedfb6c450cb9cc95d9072f8bbe28d95062fa
alon1525 <alon1525@gmail.com>
2024-05-30 18:54:52 +0300

    added some css

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
-  const [date, setDate] = useState('');
-  const [description, setDescription] = useState('');
-  const [amount, setAmount] = useState('');
-
-  const TransactionList = ({ transactions }) => {
+  
+  /*const TransactionList = ({ transactions }) => {
     const getPriceClass = (price) => {
-      if (price > 0) return 'price positive';
-      if (price < 0) return 'price negative';
-      return 'price zero';
-    };
+      if (price > 0) return "price positive";
+      if (price < 0) return "price negative";
+      return "price zero";
+    };*/
 
-  return (
-    <main>
-      <h1>400<span>.00</span></h1>
-      <form>
-        <div className='basic'>
-          <input type='text' className='name' placeholder='Transactions name'></input>
-          <input type='datetime-local' className='date'></input>
-        </div>
-        <div className='description'>
-          <input type='text' placeholder='Description'></input>
-        </div>
-        <button type='submit'>Add</button>
-      </form>
-      <div className="transactions">
-      {transactions.map((transaction, index) => (
-        <div className="transaction" key={index}>
-          <div className="left">
-            <div className="name">{transaction.name}</div>
-            <div className="description">{transaction.description}</div>
+    return (
+      <main>
+        <h1>
+          400<span>.00</span>
+        </h1>
+        <form>
+          <div className="basic">
+            <input
+              type="text"
+              className="name"
+              placeholder="Transactions name"
+            ></input>
+            <input type="datetime-local" className="date"></input>
           </div>
-          <div className="right">
-            <div className={getPriceClass(transaction.price)}>
-              {transaction.price > 0 ? `+$${transaction.price}` : `$${transaction.price}`}
+          <div className="description">
+            <input type="text" placeholder="description"></input>
+          </div>
+          <button type="submit">Add</button>
+        </form>
+        <div className="transactions">
+            <div className="transaction">
+              <div className="left">
+                <div className="name">New Samsung TV</div>
+                <div className="description">Tv i had to buy</div>
+              </div>
+              <div className="right">
+                <div className='price'>$500</div>
+                <div className="datetime">05/10/1998</div>
+              </div>
             </div>
-            <div className="datetime">{transaction.datetime}</div>
-          </div>
         </div>
-      ))}
-      </div>
-    </main>
-  );
-}
-
+      </main>
+    );
+  };
 export default App;
-
