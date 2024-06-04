64fb043a83667a243266a1f26cba94b282f15f5f
alon1525 <alon1525@gmail.com>
2024-06-04 09:24:00 +0300

    starting database and fixinig backend bugs

--- api/index.js
+++ api/index.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import cors from "cors";
+import Transaction from "models/Transaction.js";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/api/transactions', (req, res) => {
-    const transaction = req.body;
-    res.json(transaction); // Simulate saving to the database and returning the saved transaction
+app.post('/api/transaction', (req, res) => {
+    mongoose.connect('transaction')
+    const {name,description,datetime} = req.body;
+    res.json(req.body); // Simulate saving to the database and returning the saved transaction
 });
 
 app.listen(port, (req, res) => {
